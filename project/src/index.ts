db4baece6b57ec51aed58c65637859f646c1fb6d
KimDwDev <dongwan123456789@gmail.com>
2025-10-10 05:17:03 +0900

    feat: logger 추가

--- project/src/index.ts
+++ project/src/index.ts
@@ -1,3 +1,11 @@
 import "dotenv/config";
+import express, { type Express } from "express";
+import { logger } from "./config/config.logger.js";
+
+const app : Express = express();
 
 
+const port : string = process.env.NODE_APP_PORT || "8080";
+app.listen(port, () : void => {
+  logger.log("debug", "서버가 실행중입니다.");
+});
